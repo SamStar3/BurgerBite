4296c04c5390a58357fb12bd09f65af15e09d5ed
SamStar3 <samstar1044948@gmail.com>
2025-01-30 12:28:35 +0530

    file added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Barlow_Condensed} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const barlow_Condensed = Barlow_Condensed({
+  weight : ['300', '400', '500', '600', '700', '800'],
+  subsets:['latin'],
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,8 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={barlow_Condensed.className}>
         {children}
       </body>
     </html>
